6416519139d017fbab0223a999731d112b07ce4d
LENOVO USER <talyagross@gmail.com>
2023-01-16 02:46:50 +0200

    new version

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
-import win32event
+import os
+import time
+import win32process as w
 
-m = win32event.CreateMutex(None, False, 'MyMutex')
-m = win32event.OpenMutex(win32event.SYNCHRONIZE, False, 'MyMutex')
-r = win32event.WaitForSingleObject(m, 100)
-r = win32event. WaitForSingleObject(m, -1)
-win32event.ReleaseMutex(m)
+
+def main():
+    # run from terminal:  python.exe test.py
+    # os.remove('database')
+
+    startupinfo = w.STARTUPINFO()
+    print(
+        w.CreateProcess(None, "python.exe main.py get color blue", None, None, False, 0, None, None,
+                        # 0-> w.CREATE_NEW_CONSOLE
+                        startupinfo))
+    print(
+        w.CreateProcess(None, "python.exe main.py set 10000", None, None, False, 0, None, None,
+                        # 0-> w.CREATE_NEW_CONSOLE
+                        startupinfo))  # set num --> starting to set 600 (key:val) from num
+    print(
+        w.CreateProcess(None, "python.exe main.py delete 1000", None, None, False, 0, None, None,
+                        # 0-> w.CREATE_NEW_CONSOLE
+                        startupinfo))  # delete num --> starting to delete 600 (key:val) from num
+    input("Press enter to end process")
+
+    # TODO: waitforsingleobject to wait for all processes to finish before checking the data
+
+
+if __name__ == '__main__':
+    main()
+
